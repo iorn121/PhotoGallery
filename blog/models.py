a9f06c463c15cd101020adee580ed3cd4af55bde
iorn <wmt.tkn.121@gmail.com>
2022-02-10 17:40:13 +0900

    last challenge

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from cloudinary.models import CloudinaryField
 class Blog(models.Model):
     title = models.CharField(blank=False, null=False, max_length=100)
     content = models.TextField(blank=True)
-    image = models.ImageField(upload_to='image/')
+    image = models.ImageField(upload_to='image/', blank=True)
     # image = CloudinaryField('image', null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
