c5c6a7d858827a50f7cb32c98aa4f2b4af718633
iorn <wmt.tkn.121@gmail.com>
2022-02-14 16:56:04 +0900

    わからん

--- photoblog/settings.py
+++ photoblog/settings.py
@@ -169,9 +169,9 @@ if not DEBUG:
     django_heroku.settings(locals())
 
 
-@requires_csrf_token
-def my_customized_server_error(request, template_name='500.html'):
-    import sys
-    from django.views import debug
-    error_html = debug.technical_500_response(request, *sys.exc_info()).content
-    return HttpResponseServerError(error_html)
+# @requires_csrf_token
+# def my_customized_server_error(request, template_name='500.html'):
+#     import sys
+#     from django.views import debug
+#     error_html = debug.technical_500_response(request, *sys.exc_info()).content
+#     return HttpResponseServerError(error_html)
